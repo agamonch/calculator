7dcdfc2fc0d61313de26c8e08c08ed090008d7ab
agamonch <jnkyrgyz@gmail.com>
2021-04-16 13:03:59 +0300

    added math functions for calculator in MainActivity

--- app/src/main/java/kg/unicapp/calculator/MainActivity.kt
+++ app/src/main/java/kg/unicapp/calculator/MainActivity.kt
@@ -1,11 +1,62 @@
 package kg.unicapp.calculator
-
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val number1  = findViewById<EditText>(R.id.editTextTextPersonName2)
+        val number2 = findViewById<EditText>(R.id.editTextTextPersonName3)
+        val plusBtn = findViewById<Button>(R.id.button3)
+        val minusBtn = findViewById<Button>(R.id.button4)
+        val timesBtn = findViewById<Button>(R.id.button5)
+        val divBtn = findViewById<Button>(R.id.button6)
+        val result = findViewById<TextView>(R.id.textView4)
+
+        plusBtn.setOnClickListener {
+            val a = number1.text.toString().toDouble()
+            val b = number2.text.toString().toDouble()
+            val plusRes = a+b
+            result.text = "$a + $b = $plusRes "
+        }
+
+        minusBtn.setOnClickListener {
+            val a = number1.text.toString().toDouble()
+            val b = number2.text.toString().toDouble()
+            val minusRes = a - b
+            result.text = "$a - $b = $minusRes"
+        }
+
+        timesBtn.setOnClickListener {
+            val a = number1.text.toString().toDouble()
+            val b = number2.text.toString().toDouble()
+            val timesRes = a*b
+            result.text = "$a * $b = $timesRes "
+        }
+
+        divBtn.setOnClickListener {
+            val a = number1.text.toString().toDouble()
+            val b = number2.text.toString().toDouble()
+            val divRes = a/b
+            when (b){
+                0.0 -> result.text ="На ноль делить нельзя!"
+                else ->result.text = "$a / $b = $divRes "
+            }
+
+
+
+        }
+
+
+
+
+
     }
 }
